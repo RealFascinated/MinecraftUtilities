c0c4e328092a1813e07c2b031626df43dd842c67
Liam <liam@fascinated.cc>
2024-04-10 10:48:15 +0100

    use version from the pom

--- src/main/java/cc.fascinated/config/OpenAPIConfiguration.java
+++ src/main/java/cc.fascinated/config/OpenAPIConfiguration.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,16 @@ import java.util.List;
 
 @Configuration
 public class OpenAPIConfiguration {
+    /**
+     * The build properties of the
+     * app, null if the app is not built.
+     */
+    private final BuildProperties buildProperties;
+
+    @Autowired
+    public OpenAPIConfiguration(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
 
     @Bean
     public OpenAPI defineOpenAPI() {
@@ -25,7 +37,7 @@ public class OpenAPIConfiguration {
 
         Info info = new Info();
         info.setTitle("Minecraft Utilities API");
-        info.setVersion("1.0");
+        info.setVersion(buildProperties == null ? "N/A" : buildProperties.getVersion());
         info.setDescription("Wrapper for the Minecraft APIs to make them easier to use.");
         info.setContact(contact);
         info.setLicense(new License().name("MIT License").url("https://opensource.org/licenses/MIT"));
