8f1bc675960325383cc92f80bafb62c2bde3387d
Liam <liam@fascinated.cc>
2024-04-10 14:41:26 +0100

    fix tests

--- src/test/java/cc/fascinated/tests/ServerControllerTests.java
+++ src/test/java/cc/fascinated/tests/ServerControllerTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import cc.fascinated.config.TestRedisConfig;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
