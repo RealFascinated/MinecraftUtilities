69321adb1f546b146df4d1cdcb700ffa2f0ecab2
Liam <liam@fascinated.cc>
2024-04-13 21:02:54 +0100

    fix tests

--- src/test/java/xyz/mcutils/backend/tests/PlayerControllerTests.java
+++ src/test/java/xyz/mcutils/backend/tests/PlayerControllerTests.java
@@ -1,13 +1,13 @@
 package xyz.mcutils.backend.tests;
 
 import cc.fascinated.config.TestRedisConfig;
-import cc.fascinated.model.skin.ISkinPart;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import xyz.mcutils.backend.model.skin.ISkinPart;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
