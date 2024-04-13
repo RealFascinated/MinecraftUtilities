66f566027482185632aa214a7c55cc28e0100a21
Liam <liam@fascinated.cc>
2024-04-13 16:42:21 +0100

    only allow GET on /mojang/status

--- src/main/java/cc/fascinated/controller/MojangController.java
+++ src/main/java/cc/fascinated/controller/MojangController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,7 @@ public class MojangController {
     @Autowired
     private MojangService mojangService;
 
+    @ResponseBody
     @RequestMapping(value = "/status")
     public CachedEndpointStatus getStatus() {
         return mojangService.getMojangApiStatus();
