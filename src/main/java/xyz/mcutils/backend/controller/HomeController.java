2b35fd51b05bd8f11909ea283b8426edc17ee7fb
Liam <liam@fascinated.cc>
2024-04-14 17:10:40 +0100

    add bedrock example for the main page

--- src/main/java/xyz/mcutils/backend/controller/HomeController.java
+++ src/main/java/xyz/mcutils/backend/controller/HomeController.java
@@ -14,13 +14,15 @@ public class HomeController {
      * The example UUID.
      */
     private final String exampleUuid = "eeab5f8a-18dd-4d58-af78-2b3c4543da48";
-    private final String exampleServer = "aetheria.cc";
+    private final String exampleJavaServer = "aetheria.cc";
+    private final String exampleBedrockServer = "geo.hivebedrock.network";
 
     @GetMapping(value = "/")
     public String home(Model model) {
         model.addAttribute("public_url", Config.INSTANCE.getWebPublicUrl());
         model.addAttribute("player_example_url", Config.INSTANCE.getWebPublicUrl() + "/player/" + exampleUuid);
-        model.addAttribute("java_server_example_url", Config.INSTANCE.getWebPublicUrl() + "/server/java/" + exampleServer);
+        model.addAttribute("java_server_example_url", Config.INSTANCE.getWebPublicUrl() + "/server/java/" + exampleJavaServer);
+        model.addAttribute("bedrock_server_example_url", Config.INSTANCE.getWebPublicUrl() + "/server/bedrock/" + exampleBedrockServer);
         model.addAttribute("mojang_endpoint_status_url", Config.INSTANCE.getWebPublicUrl() + "/mojang/status");
         model.addAttribute("swagger_url", Config.INSTANCE.getWebPublicUrl() + "/swagger-ui.html");
         return "index";
