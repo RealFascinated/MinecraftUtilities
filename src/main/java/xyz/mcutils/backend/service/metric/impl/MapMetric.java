05d0aa500c9dd674c4838f61bb0e7210072f109b
Liam <liam@fascinated.cc>
2024-04-14 09:48:59 +0100

    fix map metric

--- src/main/java/xyz/mcutils/backend/service/metric/impl/MapMetric.java
+++ src/main/java/xyz/mcutils/backend/service/metric/impl/MapMetric.java
@@ -18,16 +18,10 @@ public class MapMetric <A, B> extends Metric<Map<A, B>> {
         for (Map.Entry<A, B> entry : getValue().entrySet()) {
             switch (entry.getValue().getClass().getSimpleName()) {
                 case "Integer":
-                    point.addField(entry.getKey().toString(), (Integer) entry.getValue());
+                    point.addField(entry.getKey().toString(), (int) entry.getValue());
                     break;
                 case "Double":
-                    point.addField(entry.getKey().toString(), (Double) entry.getValue());
-                    break;
-                case "String":
-                    point.addField(entry.getKey().toString(), (String) entry.getValue());
-                    break;
-                case "Boolean":
-                    point.addField(entry.getKey().toString(), (Boolean) entry.getValue());
+                    point.addField(entry.getKey().toString(), (double) entry.getValue());
                     break;
                 default:
                     point.addField(entry.getKey().toString(), entry.getValue().toString());
