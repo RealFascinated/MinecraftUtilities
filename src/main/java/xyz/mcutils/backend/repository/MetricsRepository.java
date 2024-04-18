02eb9e2a2e52d67cb297cf06d6104f6d8fd35b37
Liam <liam@fascinated.cc>
2024-04-18 14:05:42 +0100

    pls work

--- src/main/java/xyz/mcutils/backend/repository/MetricsRepository.java
+++ src/main/java/xyz/mcutils/backend/repository/MetricsRepository.java
@@ -1,6 +1,7 @@
 package xyz.mcutils.backend.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import xyz.mcutils.backend.service.metric.Metric;
 
 /**
@@ -8,4 +9,5 @@ import xyz.mcutils.backend.service.metric.Metric;
  *
  * @author Braydon
  */
+@EnableMongoRepositories
 public interface MetricsRepository extends MongoRepository<Metric<?>, String> { }
