b0bdf6e8008cb21f9c0edf3bc51fe3d9af5eb2b4
Liam <liam@fascinated.cc>
2024-04-18 13:56:31 +0100

    add version to embedded mongo for tests

--- src/test/java/xyz/mcutils/backend/config/TestMongoConfig.java
+++ src/test/java/xyz/mcutils/backend/config/TestMongoConfig.java
@@ -1,13 +1,7 @@
 package xyz.mcutils.backend.config;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import lombok.NonNull;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import redis.embedded.RedisServer;
-
-import java.io.IOException;
 
 /**
  * Test configuration for
--- src/test/java/xyz/mcutils/backend/tests/ServerControllerTests.java
+++ src/test/java/xyz/mcutils/backend/tests/ServerControllerTests.java
@@ -3,7 +3,6 @@ package xyz.mcutils.backend.tests;
 import cc.fascinated.config.TestRedisConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
