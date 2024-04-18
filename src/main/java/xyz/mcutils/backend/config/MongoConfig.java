984dd8bfdc337deca90ddf052d22184d58ef77e5
Liam <liam@fascinated.cc>
2024-04-18 23:38:42 +0100

    pls fix?

--- src/main/java/xyz/mcutils/backend/config/MongoConfig.java
+++ src/main/java/xyz/mcutils/backend/config/MongoConfig.java
@@ -1,8 +1,22 @@
 package xyz.mcutils.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "xyz.mcutils.backend.repository.mongo")
-public class MongoConfig { }
+public class MongoConfig {
+    @Bean
+    public MappingMongoConverter mongoConverter(MongoDatabaseFactory databaseFactory, MongoMappingContext mongoMappingContext) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(databaseFactory);
+        MappingMongoConverter mongoConverter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        mongoConverter.setMapKeyDotReplacement("-DOT");
+        return mongoConverter;
+    }
+}
